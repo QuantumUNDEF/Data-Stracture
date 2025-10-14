aac503969e359e02b3e4f0854ec6614e70cd8ed2
QuantumUNDEF <kaushendrasingh2006@gmail.com>
2025-10-14 10:47:23 +0530

    position insertion

--- Data Stracture and Algorithm/linked_list.c
+++ Data Stracture and Algorithm/linked_list.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include<stdlib.h>
 
 struct node
@@ -12,17 +12,17 @@ int main()
     // first node
     struct node*head;
     head=(struct node*)malloc(sizeof(struct node));
-    head -> data = 45;
+    head -> data = 1;
     head -> next = NULL;
     // second node
     struct node *current = malloc(sizeof(struct node));
     head -> next = current;
-    current -> data = 50;
+    current -> data = 2;
     current -> next = NULL;
     // third node
     current = malloc(sizeof(struct node));
     head -> next -> next = current;
-    current -> data = 60;
+    current -> data = 3;
     current -> next = NULL;
 
     /*printf("Our 1st node: %d\n",head->data);
@@ -36,7 +36,7 @@ int main()
     struct node *temp; // creating a temporary pointer
     temp = head;
     struct node *new_node = malloc(sizeof(struct node)); //new node
-    new_node -> data = 100;
+    new_node -> data = 4;
     new_node -> next = NULL;
     // loop to find the last element
     while (temp -> next != NULL){
@@ -46,14 +46,27 @@ int main()
     temp->next = new_node;
     // inserting the node at begining
     struct node *newHead = malloc(sizeof(struct node));
-    newHead-> data = 100;
+    newHead-> data = 5;
     newHead -> next = head;
     head = newHead;
+    // insertion at a spectic position
+    int position = 5;
+    struct node *newnode2 = malloc(sizeof(struct node));
+    newnode2 -> data = 6;
+    newnode2 -> next = NULL;
+    struct node *temp2  = head;
+    for(int i = 1; i< (position -1)&& temp2 !=NULL; i++){
+        temp2 = temp2 -> next;
+    }
+    if(temp2 != NULL){
+        newnode2-> next = temp2-> next;
+        temp2 -> next = newnode2;
+    }
     // code for printing the linked list
     struct node *ptr = head;
     while(ptr != NULL){
     printf("\n%d", ptr-> data);
-    printf("\n%d", ptr -> next);
+    //printf("\n%d", ptr -> next);
     ptr = ptr-> next;
     
     }
